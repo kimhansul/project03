ff222e8a27fdb971007c58407f368d123946fbf3
kimhansul <gkstmf7506@gmail.com>
2022-04-25 22:41:53 +0900

    파일 경로 수정/최고의 런게임 쿠키런 - 메인 슬라이드 버튼 기능 추가/푸터 - 주소 및 전화번호 등 정보 수정, 유효성 검사 링크 추가

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const headerH=$('.header').innerHeight(), fixhederH=$('.header_fixed').innerHeight(), section=$('.section');
+let autoCheck=true;
 
 $(window).on('scroll', function(){
     if($(window).scrollTop()>headerH){
@@ -17,6 +18,10 @@ $(window).on('scroll', function(){
             section.eq(i).find('.title').removeClass('active');
         }
     });
+    if($(window).scrollTop()>$('.main_slide').offset().top-300){
+        autoSlide();
+        autoCheck=false;
+    }
 });
 
 const slideTitleBox=$('.slide_title_box'), slideBox=$('.slide_box');
@@ -104,6 +109,106 @@ function slideMove(tg, start, end){
 }
 
 function autoSlide(){
-    timer=setInterval(mainSlide, 3000);
+    if(autoCheck==true){
+        timer=setInterval(mainSlide, 3000);
+    }
 }
-autoSlide();
+
+slideTitleBox.find('a').click(function(e){
+    e.preventDefault();
+    clearInterval(timer);
+});
+
+const slidePrevBtn=$('.main_prevbtn'), slideNextBtn=$('.main_nextbtn');
+
+slideNextBtn.click(function(){
+    clearInterval(timer);
+    let now=slideBox.eq(slideCurrent);
+    let prev=slideBox.eq(slideCurrent-1);
+    if(slideCurrent-1<0){
+        let subCurrent=slideBox.length+(slideCurrent-1);
+        prev=slideBox.eq(subCurrent);
+    }
+    slideMove(prev,'-100%','-200%');
+    now.removeClass('active');
+    slideMove(now, 0, '-100%');
+    titleSlide(slideCurrent);
+    slideCurrent++;
+    if(slideCurrent==slideBox.length){
+        slideCurrent=0;
+    }
+    let next=slideBox.eq(slideCurrent);
+    let nexttwo=slideBox.eq(slideCurrent+1);
+    if(slideCurrent+1==slideBox.length){
+        let subCurrent=0;
+        nexttwo=slideBox.eq(subCurrent);
+    }
+    slideMove(nexttwo,'200%','100%');
+    next.addClass('active');
+    slideMove(next, '100%', 0);
+    timer=setInterval(mainSlide, 3000);
+});
+slidePrevBtn.click(function(){
+    clearInterval(timer);
+    let now=slideBox.eq(slideCurrent);
+    let prev=slideBox.eq(slideCurrent+1);
+    if(slideCurrent+1==slideBox.length){
+        let subCurrent=0;
+        prev=slideBox.eq(subCurrent);
+    }
+    slideMove(prev,'100%', '200%');
+    now.removeClass('active');
+    slideMove(now, 0, '100%');
+    titlePrevSlide(slideCurrent);
+    slideCurrent--;
+    if(slideCurrent<0){
+        slideCurrent=slideBox.length-1;
+    }
+    let next=slideBox.eq(slideCurrent);
+    let nexttwo=slideBox.eq(slideCurrent-1);
+    if(slideCurrent-1<0){
+        let subCurrent=slideBox.length+(slideCurrent-1);
+        prev=slideBox.eq(subCurrent);
+    }
+    slideMove(nexttwo,'-200%','-100%');
+    next.addClass('active');
+    slideMove(next, '-100%', 0);
+    timer=setInterval(mainSlide, 3000);
+});
+
+function titlePrevSlide(titleCurrent){
+    let now=slideTitleBox.eq(titleCurrent);
+    let prevprev=slideTitleBox.eq(titleCurrent+2);
+    if(titleCurrent+2>=slideTitleBox.length){
+        let subCurrent=(slideTitleBox.length-(slideCurrent+2))*-1;
+        prevprev=slideTitleBox.eq(subCurrent);
+    }
+    let perv=slideTitleBox.eq(titleCurrent+1);
+    if(titleCurrent+1>=slideTitleBox.length){
+        let subCurrent=(slideTitleBox.length-(slideCurrent+1))*-1;
+        perv=slideTitleBox.eq(subCurrent);
+    }
+    let nextnextTwo=slideTitleBox.eq(titleCurrent-3);
+    if(titleCurrent-3<0){
+        let subCurrent=slideTitleBox.length+(slideCurrent-3);
+        nextnextTwo=slideTitleBox.eq(subCurrent);
+    }
+    let nextnext=slideTitleBox.eq(titleCurrent-2);
+    if(titleCurrent-2<0){
+        let subCurrent=slideTitleBox.length+(slideCurrent-2);
+        nextnext=slideTitleBox.eq(subCurrent);
+    }
+    let next=slideTitleBox.eq(titleCurrent-1);
+    if(titleCurrent-1<0){
+        let subCurrent=slideTitleBox.length+(slideCurrent-1);
+        next=slideTitleBox.eq(subCurrent);
+    }
+    slideMove(prevprev, '200%', '300%');
+    slideMove(perv, '100%', '200%');
+    now.removeClass('active');
+    slideMove(now, 0, '100%');
+    next.addClass('active');
+    slideMove(next, '-100%', 0);
+    slideMove(nextnext, '-200%', '-100%');
+    slideMove(nextnextTwo, '-300%', '-200%');
+}
